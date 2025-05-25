cebbbd2bc8da3be10cd58916bdcca3a40db222e4
kevellyoliveira <kevelly.oliveira@sptech.school>
2025-05-24 23:01:01 -0300

    Adicionado: entidade, controlador e teste unitário para Cargo

---
+++ backEnd/api-mocuti/src/main/kotlin/com/api/mocuti/controller/CanalComunicacaoJpaController.kt
@@ -0,0 +1,25 @@
+package com.api.mocuti.controller
+
+import com.api.mocuti.entity.CanalComunicacao
+import com.api.mocuti.entity.Cargo
+import com.api.mocuti.repository.CanalComunicacaoRepository
+import com.api.mocuti.repository.CargoRepository
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/canal-comunicacao")
+class CanalComunicacaoJpaController (var repositorioCanalComunicacao: CanalComunicacaoRepository) {
+    @GetMapping
+    fun get(): ResponseEntity<List<CanalComunicacao>> {
+        val canais = repositorioCanalComunicacao.findAll()
+
+        return if (canais.isEmpty()) {
+            ResponseEntity.status(204).build()
+        } else {
+            ResponseEntity.status(200).body(canais)
+        }
+    }
+}
---
+++ backEnd/api-mocuti/src/main/kotlin/com/api/mocuti/repository/CanalComunicacaoRepository.kt
@@ -0,0 +1,7 @@
+package com.api.mocuti.repository
+
+import com.api.mocuti.entity.CanalComunicacao
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface CanalComunicacaoRepository: JpaRepository<CanalComunicacao, Int> {
+}
