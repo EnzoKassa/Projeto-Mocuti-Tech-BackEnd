74d2dbfb1d7d6236b149472379a479b8831d6dc0
kevellyoliveira <kevelly.oliveira@sptech.school>
2025-05-25 12:37:30 -0300

    Adicionado: entidade, controlador e teste unitário para StatusInscricao

---
+++ backEnd/api-mocuti/src/main/kotlin/com/api/mocuti/entity/StatusInscricao.kt
@@ -0,0 +1,20 @@
+package com.api.mocuti.entity
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Size
+
+@Entity
+data class StatusInscricao (
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    var id: Int? = null,
+    @field:NotBlank @field:Size(min = 2, max = 45)
+    var tipoInscricao: String? = null
+) {
+}
