71d142980899839c416c6456101ad13b223d6c3b
kevellyoliveira <kevelly.oliveira@sptech.school>
2025-09-14 12:57:49 -0300

    utilizando .env para rodar a api

--- backEnd/api-mocuti/src/main/kotlin/com/api/mocuti/ApiMocutiApplication.kt
+++ backEnd/api-mocuti/src/main/kotlin/com/api/mocuti/ApiMocutiApplication.kt
@@ -1,5 +1,6 @@
 package com.api.mocuti
 
+import io.github.cdimascio.dotenv.Dotenv
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -7,5 +8,15 @@ import org.springframework.boot.runApplication
 class ApiMocutiApplication
 
 fun main(args: Array<String>) {
+	// Carrega o .env da raiz do projeto
+	val dotenv = Dotenv.configure()
+		.ignoreIfMissing() // não dá erro se não existir (ex: produção)
+		.load()
+
+	// Joga as variáveis do .env como System properties
+	dotenv.entries().forEach { entry ->
+		System.setProperty(entry.key, entry.value)
+	}
+
 	runApplication<ApiMocutiApplication>(*args)
 }
